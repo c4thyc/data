e35dbbecd1cbfe7c90ee8ce9d6ff8c91037ed5da
c4thyc <cathyc22@nycstudents.net>
2025-05-01 16:33:28 +0000

    repopulate working on it

---
+++ Data.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+public class Data {
+    public static final int MAX = (int) (Math.random() * Integer.MAX_VALUE);
+    private int[][] grid;
+
+    public Data(int[][] grid) {
+        this.grid = grid;
+    }
+
+    /**
+     * Fills all elements of grid with randomly generated
+     * values, as described in part (a)
+     * Precondition: grid is not null.
+     * grid has at least one element.
+     */
+    public void repopulate() {
+        for (int i = 0; i < data[0].length; i++) {
+            for (int f = 0; f < data.length; f++) {
+                
+            }
+        }
+    }
+
+
+    /**
+     * Returns the number of columns in
+     * grid that are in increasing order, as
+     * described in part (b)
+     * Precondition: grid is not null.
+     * grid has at least one element.
+     */
+    public int countIncreasingCols() {
+        /* to be implemented in part (b) */
+    }
+
+
+    // There may be instance variables, constructors
+    //and methods that are not shown.
+}
