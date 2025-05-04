193463b6ffe28df8e553cdd9372b6f24339fe33b
c4thyc <cathyc22@nycstudents.net>
2025-05-04 04:19:26 +0000

    done

--- Data.java
+++ Data.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 public class Data {
     public static final int MAX = (int) (Math.random() * Integer.MAX_VALUE);
     private int[][] grid;
@@ -14,9 +13,13 @@ public class Data {
      * grid has at least one element.
      */
     public void repopulate() {
-        for (int i = 0; i < data[0].length; i++) {
-            for (int f = 0; f < data.length; f++) {
-                
+        for (int i = 0; i < grid[0].length; i++) {
+            for (int f = 0; f < grid.length; f++) {
+                int num = 0;
+                while (num % 10 == 0 && num % 100 != 0) {
+                    num = (int) (Math.random() * MAX) + 1;
+                }
+                grid[f][i] = num;
             }
         }
     }
@@ -30,9 +33,19 @@ public class Data {
      * grid has at least one element.
      */
     public int countIncreasingCols() {
-        /* to be implemented in part (b) */
+        int count = 0;
+        if (grid.length == 1) return 1;
+        for (int col = 0; col < grid[0].length; col++) {
+            boolean increasing = true;
+            for (int row = 0; row < grid.length - 1; row++) {
+                if (grid[row][col] > grid[row + 1][col]) {
+                    increasing = false;
+                }
+            }
+            if (increasing) count++;
+        }
+        return count;
     }
-
 
     // There may be instance variables, constructors
     //and methods that are not shown.
